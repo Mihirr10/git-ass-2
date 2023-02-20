d214c2ae626edd43c904067f9ea4571a3f11fcd0
mihir <mihirparmar031@gmail.com>
2023-02-20 13:07:59 +0530

    Ass-2, modified demo2.java

--- Demo2.java
+++ Demo2.java
@@ -1,7 +1,7 @@
 class Demo2{  
     public static void main(String args[]){  
      int i,fact=1;  
-     int number=10;//It is the number to calculate factorial    
+     int number=15;//It is the number to calculate factorial    
      for(i=1;i<=number;i++){    
          fact=fact*i;    
      }    
